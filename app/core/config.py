a78b93668c2fd08946a33d402f785a61bff3b892
lucasgrass <grassberaldo@gmail.com>
2025-01-14 20:24:29 -0300

    Removing .gitignore configuration

---
+++ app/core/config.py
@@ -0,0 +1,10 @@
+class Settings:
+    DATABASE_URL: str = "postgresql+asyncpg://postgres:140901@localhost:5432/RegistraNota"
+    API_STR: str = "/api/v1"
+
+    FIRST_SUPERUSER_EMAIL: str = "grassberaldo@gmail.com"
+    FIRST_SUPERUSER_PASSWORD: str = "nossoprojeto123"
+    FIRST_SUPERUSER_CODIGO: str = "01/01"
+
+
+settings = Settings()
