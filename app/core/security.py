04012d2fc1c6828cb336b3bb97386679d4cfd090
lucasgrass <grassberaldo@gmail.com>
2025-01-15 22:01:40 -0300

    Changing JWT time

--- app/core/security.py
+++ app/core/security.py
@@ -35,7 +35,7 @@ def create_access_token(data: dict, expires_delta: Union[timedelta, None] = None
 
 def create_refresh_token(data: dict):
     to_encode = data.copy()
-    expire = datetime.utcnow() + timedelta(days=30)
+    expire = datetime.utcnow() + timedelta(days=REFRESH_TOKEN_EXPIRE_DAYS)
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
